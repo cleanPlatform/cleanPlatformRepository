8260c825b441ab3179cc4252a2852852aeca1e9f
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-08-18 09:59:38 +0900

    company 업체 관련 로직 틀 작성

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-const router = require('./0routes')
+const router = require('./0routes');
 
-app.use(express.json())
-app.use(cors({ origin: true, credientials: true }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use('/api', router)
+app.use(express.json());
+app.use(cors({ origin: true, credientials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use('/api', router);
 
 app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
-  error.status = 404
-  next(error)
-})
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
 
 app.use((err, req, res, next) => {
-  console.error(err)
-  res.status(err.status || 500).json({ message: err })
-})
+  console.error(err);
+  res.status(err.status || 500).json({ message: err });
+});
 
 app.listen(PORT, () => {
-  console.log(PORT, '포트 번호로 서버가 실행되었습니다.')
-})
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+});
