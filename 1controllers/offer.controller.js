028ee19f217b56348a783caf54720d4a50e06105
hoonbongEE <gnswjd4422@gmail.com>
2023-08-19 19:16:39 +0900

    feat: offer crud로직 구현 offer 관련 주석 제거 offer 모델 companyId 주석 app.js의 sequelize 로직 주석

--- 1controllers/offer.controller.js
+++ 1controllers/offer.controller.js
@@ -1,10 +1,62 @@
-const OfferRepository = require('../3repositories/offer.repository');
-const ApiError = require('../apierror');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const OfferService = require('../2services/offer.service');
 
-class OfferService {
-  offerRepository = new OfferRepository();
+class OfferController {
+  offerService = new OfferService();
+
+  // 업체 서비스 생성
+  createOffer = async (req, res, next) => {
+    const { offerName, offerNumber, price } = req.body;
+
+    //const { companyId } = req.params;
+
+    // const userId = res.locals.user
+
+    const { status, message } = await this.offerService.createOffer(offerName, offerNumber, price);
+    res.status(status).json(message);
+  };
+
+  // 업체 서비스 수정
+  putOffer = async (req, res, next) => {
+    const { offerName, offerNumber, price } = req.body;
+
+    const { companyId, offerId } = req.params;
+
+    // const userId = res.locals.user
+
+    const { status, message } = await this.offerService.updateOffer(
+      offerId,
+      offerName,
+      offerNumber,
+      price
+    );
+    res.status(status).json(message);
+  };
+
+  // 업체 서비스 삭제
+  deleteOffer = async (req, res, next) => {
+    const { offerId } = req.params;
+
+    // const userId = res.locals.user
+
+    const { status, message } = await this.offerService.destroyOffer(offerId);
+    res.status(status).json(message);
+  };
+
+  // 업체 서비스 전체 조회
+  getOffer = async (req, res, next) => {
+    const findAllMessage = await this.offerService.findAllOffer();
+    res.status(200).json({ data: findAllMessage });
+  };
+
+  // 업체 서비스 상세 조회
+  onlyget = async (req, res, next) => {
+    const { offerId } = req.params;
+
+    // const userId = res.locals.user
+
+    const findOneMessage = await this.offerService.findOneOffer(offerId);
+    res.status(200).json({ data: findOneMessage });
+  };
 }
 
-module.exports = OfferService;
+module.exports = OfferController;
