2a7254c54ba52e4d76f552d77333d84d76bfab66
MinSamJun <116451120+MinSamJun@users.noreply.github.com>
2023-08-29 10:01:10 +0900

    Update login.router.js

--- 0router/login.router.js
+++ 0router/login.router.js
@@ -6,7 +6,7 @@ const loginController = new LoginController();
 
 const authMiddleware = require('../middlewares/auth-middleware');
 
-router.post('/signIn', loginController.loginController);
+router.post('/signin', loginController.loginController);
 router.post('/signout', loginController.logoutController);
 
 module.exports = router;
