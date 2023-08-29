67a19663c10ebfb5c0c6e2c8387e750c3e659777
MinSamJun <116451120+MinSamJun@users.noreply.github.com>
2023-08-29 10:04:24 +0900

    Update login.service.js

--- 2services/login.service.js
+++ 2services/login.service.js
@@ -7,7 +7,7 @@ const UserRepository = require('../3repositories/user.repository');
 class LoginService {
   userRepository = new UserRepository();
   //  로그인 매서드
-  loginUserService = async (email, password) => {
+  loginUser = async (email, password) => {
     try {
       // 존재하는 이메일인지 확인하기
       const isExistUser = await this.userRepository.findUser(email);
